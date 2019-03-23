e68275bcfac4ba43b46c941cbcfbf62eeadab436
SmallMuscle <SmallMuscle_liang@163.com>
2019-03-23 12:11:15 +0800

    style: 2019.03.21

--- src/main/java/L_929_UniqueEmailAddresses.java
+++ src/main/java/L_929_UniqueEmailAddresses.java
@@ -4,41 +4,41 @@ public class L_929_UniqueEmailAddresses {
 
 
     /*
-    2019.03.21
-    Every email consists of a local name and a domain name, separated
-    by the @ sign.
+        2019.03.21
 
-    For example, in alice@leetcode.com, alice is the local name, and
-    leetcode.com is the domain name.
+        Every email consists of a local name and a domain name, separated
+        by the @ sign.
 
-    Besides lowercase letters, these emails may contain '.'s or '+'s.
+        For example, in alice@leetcode.com, alice is the local name, and
+        leetcode.com is the domain name.
 
-    If you add periods ('.') between some characters in the local name
-    part of an email address, mail sent there will be forwarded to the
-    same address without dots in the local name.  For example, "alice.z@leetcode.com"
-    and "alicez@leetcode.com" forward to the same email address.
-    (Note that this rule does not apply for domain names.)
+        Besides lowercase letters, these emails may contain '.'s or '+'s.
 
-    If you add a plus ('+') in the local name, everything after the first
-    plus sign will be ignored. This allows certain emails to be filtered,
-    for example m.y+name@email.com will be forwarded to my@email.com.
-    (Again, this rule does not apply for domain names.)
+        If you add periods ('.') between some characters in the local name
+        part of an email address, mail sent there will be forwarded to the
+        same address without dots in the local name.  For example, "alice.z@leetcode.com"
+        and "alicez@leetcode.com" forward to the same email address.
+        (Note that this rule does not apply for domain names.)
 
-    It is possible to use both of these rules at the same time.
+        If you add a plus ('+') in the local name, everything after the first
+        plus sign will be ignored. This allows certain emails to be filtered,
+        for example m.y+name@email.com will be forwarded to my@email.com.
+        (Again, this rule does not apply for domain names.)
 
-    Given a list of emails, we send one email to each address in the list.
-    How many different addresses actually receive mails?
+        It is possible to use both of these rules at the same time.
 
-    Example 1:
-        Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com",
-            "testemail+david@lee.tcode.com"]
-        Output: 2
-        Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
-    Note:
-        1 <= emails[i].length <= 100
-        1 <= emails.length <= 100
-        Each emails[i] contains exactly one '@' character.
+        Given a list of emails, we send one email to each address in the list.
+        How many different addresses actually receive mails?
 
+        Example 1:
+            Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com"]
+            Output: 2
+            Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+        Note:
+            1 <= emails[i].length <= 100
+            1 <= emails.length <= 100
+            Each emails[i] contains exactly one '@' character.
 
      */
 
@@ -71,6 +71,7 @@ public class L_929_UniqueEmailAddresses {
     /*
     * dealString1、dealString3 大同小异
     * dealString2 增加处理 连续出现 . 的情况，相对于 1、3 减少 copy 次数，不确定输入的数据是否存在这种类型的。。。。
+    * dealString4 更好理解，效率也不错
     * */
     public String dealString4(String dest) {
         char[] chs = new char[dest.length()];
