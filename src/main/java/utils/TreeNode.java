e3bcbdf703e53682bf26487306e01dbb50935812
zhangdl <zhangdl@otmsg.com>
2019-03-28 18:58:43 +0800

    feat: 2019.03.28 回家陪女朋友。。未来三天暂停  = =

---
+++ src/main/java/utils/TreeNode.java
@@ -0,0 +1,36 @@
+package utils;
+
+public class TreeNode {
+
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode getLeft() {
+                return left;
+        }
+
+        public void setLeft(TreeNode left) {
+                this.left = left;
+        }
+
+        public TreeNode getRight() {
+                return right;
+        }
+
+        public void setRight(TreeNode right) {
+                this.right = right;
+        }
+
+        public TreeNode(int x) {
+            val = x;
+        }
+
+        public void setVal(int val) {
+                this.val = val;
+        }
+
+        public int getVal() {
+                return val;
+        }
+}
