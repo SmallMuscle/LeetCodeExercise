51fc0983d71d71de9342ba3c24a173f74b0ecc55
zhangdl <zhangdl@otmsg.com>
2019-05-14 14:22:17 +0800

    feat: 136、412、575

---
+++ src/main/java/DifficultSequence/Easy/L_412_FizzBuzz.java
@@ -0,0 +1,95 @@
+package DifficultSequence.Easy;
+
+import utils.PrintUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L_412_FizzBuzz {
+
+    /*
+        Write a program that outputs the string representation of
+        numbers from 1 to n.
+
+        But for multiples of three it should output “Fizz” instead
+        of the number and for the multiples of five output “Buzz”.
+        For numbers which are multiples of both three and five
+        output “FizzBuzz”.
+
+        Example:
+
+        n = 15,
+
+        Return:
+        [
+            "1",
+            "2",
+            "Fizz",
+            "4",
+            "Buzz",
+            "Fizz",
+            "7",
+            "8",
+            "Fizz",
+            "Buzz",
+            "11",
+            "Fizz",
+            "13",
+            "14",
+            "FizzBuzz"
+        ]
+     */
+
+    public static void main(String[] args) {
+        L_412_FizzBuzz l = new L_412_FizzBuzz();
+        PrintUtil.printList(l.fizzBuzz(15));
+    }
+
+    public List<String> fizzBuzz(int n) {
+        return fizzBuzz2(n);
+    }
+
+    private static final String three = "Fizz";
+
+    private static final String five = "Buzz";
+
+    private static final String threeFive = "FizzBuzz";
+
+
+    public List<String> fizzBuzz1(int n) {
+        List<String> list = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            if (0 == (i % 5) && 0 == (i % 3)) list.add(threeFive);
+            else if (0 == (i % 3)) list.add(three);
+            else if (0 == (i % 5)) list.add(five);
+            else list.add(Integer.toString(i));
+        }
+        return list;
+    }
+
+    // inspired by Discuss
+    public List<String> fizzBuzz2(int n) {
+        List<String> list = new ArrayList<>(n);
+        int threeIndex = 3;
+        int fiveIndex = 5;
+        for (int i = 1; i <= n; i++) {
+            if (i == fiveIndex && i == threeIndex) {
+                list.add(threeFive);
+                threeIndex += 3;
+                fiveIndex += 5;
+            }
+            else if (threeIndex == i) {
+                list.add(three);
+                threeIndex += 3;
+            }
+            else if (fiveIndex == i) {
+                list.add(five);
+                fiveIndex += 5;
+            }
+            else list.add(Integer.toString(i));
+        }
+        return list;
+    }
+
+
+}
---
+++ src/main/java/DifficultSequence/Easy/L_575_DistributeCandies.java
@@ -0,0 +1,65 @@
+package DifficultSequence.Easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class L_575_DistributeCandies {
+
+    /*
+        Given an integer array with even length, where different numbers
+        in this array represent different kinds of candies. Each number
+        means one candy of the corresponding kind. You need to distribute
+        these candies equally in number to brother and sister. Return the
+        maximum number of kinds of candies the sister could gain.
+
+        Example 1:
+            Input: candies = [1,1,2,2,3,3]
+            Output: 3
+            Explanation:
+                There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+                Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too.
+                The sister has three different kinds of candies.
+        Example 2:
+            Input: candies = [1,1,2,3]
+            Output: 2
+            Explanation: For example, the sister has candies [2,3] and the brother has candies [1,1].
+                The sister has two different kinds of candies, the brother has only one kind of candies.
+        Note:
+            The length of the given array is in range [2, 10,000], and will be even.
+            The number in given array is in range [-100,000, 100,000].
+     */
+
+    public static void main(String[] args) {
+        L_575_DistributeCandies l = new L_575_DistributeCandies();
+        int[] candies1 = {1,1,2,2,3,3};
+        System.out.println(l.distributeCandies(candies1));
+        int[] candies2 = {1,1,2,3};
+        System.out.println(l.distributeCandies(candies2));
+    }
+
+    public int distributeCandies(int[] candies) {
+        return distributeCandies2(candies);
+    }
+
+    public int distributeCandies1(int[] candies) {
+        int num = candies.length >>  1;
+        Set<Integer> set = new HashSet<>();
+        for (int c : candies) {
+            set.add(c);
+        }
+        return num > set.size() ? set.size() : num;
+    }
+
+    public int distributeCandies2(int[] candies) {
+        int[] index = new int[200001];
+        int num = candies.length >> 1;
+        int type = 0;
+        for (int c : candies) {
+            index[c + 100000] = 1;
+        }
+        for (int i : index) {
+            if (1 == i) ++type;
+        }
+        return num > type ? type : num;
+    }
+}
