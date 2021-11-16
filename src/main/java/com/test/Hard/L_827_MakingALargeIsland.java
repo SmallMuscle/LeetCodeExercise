c59c57c8e43b4415258bfff8c40ee98c0736474f
zhangdl <zhangdl@thunisoft.com>
2021-11-16 09:48:19 +0800

    feat: 827 design

--- src/main/java/com/test/Hard/L_827_MakingALargeIsland.java
+++ src/main/java/com/test/Hard/L_827_MakingALargeIsland.java
@@ -1,5 +1,11 @@
 package com.test.Hard;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class L_827_MakingALargeIsland {
 
     /**
@@ -23,6 +29,40 @@ public class L_827_MakingALargeIsland {
      * 1 <= n <= 500
      *
      * analysys:
+     * 遍历出各个相连的 1，
+     * 判断边界 +1 可以连接的区域
+     * 计算最大值
+     *
+     *
+     * 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0
+     * 0 0 0 0 0 0 0 1 1 1 1 1 0 0 0 0
+     * 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0
+     * 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 0
+     * 0 0 0 1 1 1 1 1 1 1 1 1 1 0 0 0
+     * 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0
+     * 0 0 0 0 0 0 1 1 1 1 1 1 1 1 0 0
+     * 0 0 0 0 0 1 1 1 1 1 1 1 1 0 0 0
+     * 0 0 0 0 1 1 0 0 0 1 1 1 0 0 0 0
+     * 0 0 0 0 1 1 1 0 0 0 1 0 0 0 0 0
      *
      */
+    public int largestIsland(int[][] grid) {
+        return 0;
+    }
+
+    private List<Island> findIsland(int[][] grid) {
+        List<Island> islands = new ArrayList<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+            }
+        }
+        return islands;
+    }
+
+    class Island {
+        Map<Integer, Integer> border;
+        int num;
+    }
 }
