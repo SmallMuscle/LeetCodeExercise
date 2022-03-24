343bd6e81aea6d7a9c397a586da2d6478690ac1f
zhangdl <zhangdl@thunisoft.com>
2022-03-24 08:36:43 +0800

    调整计时 func；

--- src/main/java/com/test/utils/CostTimeUtil.java
+++ src/main/java/com/test/utils/CostTimeUtil.java
@@ -8,9 +8,9 @@ import java.util.function.Supplier;
 @Slf4j
 public class CostTimeUtil {
 
-    public static Object costMillisecond(Supplier supplier, Object ... args) {
+    public static <R> R costMillisecond(Supplier<R> supplier, Object ... args) {
         long start = System.nanoTime();
-        Object result = supplier.get();
+        R result = supplier.get();
         log.info("cost time {} ns", System.nanoTime() - start);
         return result;
     }
--- src/test/java/com/test/Hard/L_827_MakingALargeIsland/SolutionTest.java
+++ src/test/java/com/test/Hard/L_827_MakingALargeIsland/SolutionTest.java
@@ -5,65 +5,61 @@ import com.test.utils.PrintUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.util.function.Consumer;
-
 @Slf4j
 public class SolutionTest {
 
+    private Solution solution = new Solution();
 
     @Test
     public void test() {
-        Solution solution = new Solution();
-        int[][] grid = new int[][] {{0, 0},
-                                    {0, 0}};
+        final int[][] grid1 = new int[][]{
+                {0, 0},
+                {0, 0}};
+        execTest(grid1);
+        final int[][] grid2 = new int[][]{
+                {0, 1},
+                {1, 0}};
+        execTest(grid2);
+        final int[][] grid3 = new int[][]{
+                {1, 1},
+                {1, 0}};
+        execTest(grid3);
+        final int[][] grid4 = new int[][]{
+                {1, 1},
+                {1, 1}};
+        execTest(grid4);
+        final int[][] grid5 = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 0, 0},
+                {0, 1, 0, 0, 1, 0, 0},
+                {1, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 0, 0},
+                {0, 1, 1, 1, 1, 0, 0}};
+        execTest(grid5);
+        final int[][] grid6 = new int[][]{
+                {1, 0, 1, 0, 1},
+                {0, 1, 1, 0, 1},
+                {1, 1, 1, 0, 0},
+                {1, 0, 1, 1, 1},
+                {0, 0, 1, 1, 0}};
+        execTest(grid6);
+        final int[][] grid7 = new int[][] {
+                {1,0,0,0,1,0,1,0,1},
+                {0,1,0,0,1,0,0,0,0},
+                {1,0,1,0,1,1,0,0,0},
+                {1,1,1,1,1,0,0,0,0},
+                {1,1,0,1,0,1,1,1,0},
+                {0,0,0,1,0,1,0,1,1},
+                {1,1,1,1,0,0,1,1,0},
+                {0,1,1,1,0,1,0,0,1},
+                {1,1,1,0,1,0,1,0,1}};
+        execTest(grid7);
+    }
+
+    private void execTest(int [][] grid) {
         PrintUtil.printArray(grid);
-        final int [][] g = grid;
-        int max = (int) CostTimeUtil.costMillisecond(() -> solution.largestIsland(g));
-        log.info("max island: {}", max);
-        grid = new int[][] {{0, 1},
-                            {1, 0}};
-        PrintUtil.printArray(grid);
-        max = solution.largestIsland(grid);
-        log.info("max island: {}", max);
-        grid = new int[][] {{1, 1},
-                            {1, 0}};
-        PrintUtil.printArray(grid);
-        max = solution.largestIsland(grid);
-        log.info("max island: {}", max);
-        grid = new int[][] {{1, 1},
-                            {1, 1}};
-        PrintUtil.printArray(grid);
-        max = solution.largestIsland(grid);
-        log.info("max island: {}", max);
-        grid = new int[][] {{0,0,0,0,0,0,0},
-                            {0,1,1,1,1,0,0},
-                            {0,1,0,0,1,0,0},
-                            {1,0,1,0,1,0,0},
-                            {0,1,0,0,1,0,0},
-                            {0,1,0,0,1,0,0},
-                            {0,1,1,1,1,0,0}};
-        PrintUtil.printArray(grid);
-        max = solution.largestIsland(grid);
-        log.info("max island: {}", max);
-        grid = new int[][] {{1,0,1,0,1},
-                            {0,1,1,0,1},
-                            {1,1,1,0,0},
-                            {1,0,1,1,1},
-                            {0,0,1,1,0}};
-        PrintUtil.printArray(grid);
-        max = solution.largestIsland(grid);
-        log.info("max island: {}", max);
-        grid = new int[][] {{1,0,0,0,1,0,1,0,1},
-                            {0,1,0,0,1,0,0,0,0},
-                            {1,0,1,0,1,1,0,0,0},
-                            {1,1,1,1,1,0,0,0,0},
-                            {1,1,0,1,0,1,1,1,0},
-                            {0,0,0,1,0,1,0,1,1},
-                            {1,1,1,1,0,0,1,1,0},
-                            {0,1,1,1,0,1,0,0,1},
-                            {1,1,1,0,1,0,1,0,1}};
-        PrintUtil.printArray(grid);
-        max = solution.largestIsland(grid);
+        int max = CostTimeUtil.costMillisecond(() -> solution.largestIsland(grid));
         log.info("max island: {}", max);
     }
 }
