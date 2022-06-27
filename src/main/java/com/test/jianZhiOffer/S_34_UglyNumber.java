7b15b188be8bdf25b10405cb54234eb9eac9fb1f
zhangdl <zhangdeliang@thunisoft.com>
2022-06-27 19:47:38 +0800

    剑指Offer 34 form

---
+++ src/main/java/com/test/jianZhiOffer/S_34_UglyNumber.java
@@ -0,0 +1,15 @@
+package com.test.jianZhiOffer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class S_34_UglyNumber {
+
+    public int uglyNumber(int num) {
+        if (1 > num) return -1;
+        int
+    }
+
+}
---
+++ src/test/java/com/test/jianZhiOffer/S_34_UglyNumberTest.java
@@ -0,0 +1,50 @@
+package com.test.jianZhiOffer;
+
+import com.test.utils.CostTimeUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class S_34_UglyNumberTest {
+
+    private S_34_UglyNumber solution = new S_34_UglyNumber();
+
+    @Test
+    public void uglyNumberTest() {
+        int n = -1;
+        execTest(n, -1);
+        n = 0;
+        execTest(n, -1);
+        n = 1;
+        execTest(n, 2);
+        n = 2;
+        execTest(n, 3);
+        n = 3;
+        execTest(n, 4);
+        n = 4;
+        execTest(n, 5);
+        n = 5;
+        execTest(n, 6);
+        n = 6;
+        execTest(n, 8);
+        n = 7;
+        execTest(n, 9);
+        n = 8;
+        execTest(n, 10);
+        n = 9;
+        execTest(n, 12);
+    }
+
+    public void execTest(int n, int expect) {
+        log.info("-------------------------------------------------");
+        try {
+            int result = CostTimeUtil.costMillisecond(() -> solution.uglyNumber(n));
+            log.info("The {} ugly number is {}", n, result);
+            log.info("exec result {}correct.", expect == result ? "" : "in");
+        } catch (Exception e) {
+            log.error("err: ", e);
+        }
+    }
+
+
+}
